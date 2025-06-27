81b0247d63c2defeb2a819b66bf0731ac1f25ff6
dream2hvn <138209676+dream2hvn@users.noreply.github.com>
2025-06-27 14:08:52 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,8 +2,10 @@ import mlflow
 import mlflow.sklearn
 import numpy as np
 import pandas as pd
+import argparse
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+
 
 # Argument parsing
 parser = argparse.ArgumentParser()
